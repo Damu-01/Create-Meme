09702ec0c30da69e8c361e227afa4ed5ad71d111
Damu-01 <damuingle@gmail.com>
2023-11-30 13:58:18 +0530

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+const Header = () =>  {
+    return (
+        <header className="header">
+            <img 
+                src="https://play-lh.googleusercontent.com/Oe8twVdZFIyM5Htji5awa_wVV2IyU6O5vZKjTh4LmUbzAG2Q4iM2FBZAS_l4NazoCaY" 
+                className="header--image"
+            />
+            <h2 className="header--title">Meme Generator</h2>
+            <h4 className="header--project">React Project </h4>
+        </header>
+    )
+}
+export default Header;
